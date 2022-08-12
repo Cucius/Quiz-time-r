288af445ba38a2fac64b38cc338b0c05caa3eb25
Cucius <Rylan19@Gmail.com>
2022-08-11 21:45:01 -0600

    Added a working timer when start button is pushed

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector(".timer-count");
+
+var timer;
+var timerCount;
 
 startButton.addEventListener("click", startGame);
 
+//Start game
 function startGame() {
   timerCount = 100;
   startButton.disabled = true;
-  //   startTimer();
+  startTimer();
   //   genQuestion();
   console.log("clicked");
+}
+
+function startTimer() {
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
