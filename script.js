812d54b376d7f8a69e75c925d4bfc13a98f4b35e
Cucius <Rylan19@Gmail.com>
2022-08-12 17:12:28 -0600

    Fixed bugs and refactored the code

--- script.js
+++ script.js
@@ -1,130 +1,138 @@
 var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".timer-count");
 var getQuiz = document.querySelector(".quiz");
-var getAnswers = document.querySelector(".answers");
-
 var favoriteEl = document.createElement("div");
 
 var timer;
-var timerCount;
+var timerCount = 60;
 
 var totalPoints = 0;
 
-var quizQuestion = 0;
-
 var question = ["What is always coming but never arrives?", "What gets wetter the more it dries?", "What is it that lives if it is fed, and dies if you give it a drink?", "What never asks a question but gets answered all the time?", "What word would you use to describe a man who does not have all his fingers on one hand?"];
+var answers = ["Tomorrow", "A towel", "Fire", "A Phone", "Normal", "Odd", "Freak"];
 
-var quizAnswers = 0;
-var answers = ["Tomorrow", "A towel", "Fire", "A Phone", "Normal"];
-
-// var rightAnswer = true;
-// var wrongAnswer = false;
-
-// var wrongAnswer1 = false;
-// var wrongAnswer2 = false;
+var numAnswers = 0;
+var numQuestion = 0;
 
 //Start game
 function startGame() {
   timerCount = 60;
+  totalPoints = 0;
+  numAnswers = 0;
+  numQuestion = 0;
+
   startButton.disabled = true;
   startTimer();
   genQuestion();
   genAnswers();
 }
 
+function deleteAnswerButtons() {
+  document.getElementById("answerButtonsLocation").textContent = "";
+  favoriteEl = document.createElement("div");
+}
+
+function quizOver() {
+  clearInterval(timer);
+  deleteAnswerButtons();
+
+  timerElement.textContent = "Game Over";
+  getQuiz.textContent = "Done! Points: " + totalPoints;
+
+  startButton.disabled = false;
+}
+
+function nextRound() {
+  // delete answer buttons so new ones can be made
+  deleteAnswerButtons();
+
+  genQuestion();
+  genAnswers();
+
+  if (numQuestion > 5) {
+    quizOver();
+  }
+}
+
 //Timer function
 function startTimer() {
   timer = setInterval(function () {
     timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount === 0) {
+    // timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      displayTimer();
+    } else {
+      timerElement.textContent = "";
       clearInterval(timer);
-    } else if (timerCount <= 20) {
+      quizOver();
+    }
+
+    if (timerCount <= 20) {
       timerElement.setAttribute("style", "color: var(--active)");
     }
-  }, 1000);
+  }, 500);
+}
+
+function subtractTimeFromTimer() {
+  clearInterval(timer);
+  timerCount = timerElement.textContent - 10;
+  startTimer();
+}
+
+function displayTimer() {
+  timerElement.textContent = timerCount;
 }
 
 //Generate questions
 function genQuestion() {
-  var numQuestion = 0;
-  genAnswers();
   getQuiz.textContent = question[numQuestion];
+  numQuestion++;
 }
+
 function genAnswers() {
-  document.body.children[0].children[1].children[4].appendChild(favoriteEl);
+  var choiceOne = document.createElement("button");
+  var choiceTwo = document.createElement("button");
+  var choiceThree = document.createElement("button");
+
+  favoriteEl.appendChild(choiceOne);
+  favoriteEl.appendChild(choiceTwo);
+  favoriteEl.appendChild(choiceThree);
+
+  //TODO: CHANGE THE ANSWER DISPLAYED IN THE choice button
+  choiceOne.textContent = answers[numAnswers];
+  choiceTwo.textContent = answers[numAnswers + 1];
+  choiceThree.textContent = answers[numAnswers + 2];
+
+  choiceOne.setAttribute("class", "answers");
+  choiceTwo.setAttribute("class", "answers");
+  choiceThree.setAttribute("class", "answers");
+
+  // TODO: INSERT IF STATEMENT TO SELECT CORRECT BUTTON FOR correctAnswer
+  // to randomize answer location
+  choiceOne.addEventListener("click", correctAnswer);
+  choiceTwo.addEventListener("click", wrongAnswer);
+  choiceThree.addEventListener("click", wrongAnswer);
+
+  document.getElementById("answerButtonsLocation").appendChild(favoriteEl);
+  numAnswers++;
 }
 
-var numAnswers = 0;
+function correctAnswer() {
+  console.log("CORRECT ANSWER");
+  totalPoints++;
+  nextRound();
+}
 
-var li1 = document.createElement("button");
-var li2 = document.createElement("button");
-var li3 = document.createElement("button");
+function wrongAnswer() {
+  console.log("WRONG ANSWER");
+  totalPoints--;
+  // timerElement.textContent = timerCount - 10;
+  subtractTimeFromTimer();
+  // timerElement.
 
-favoriteEl.appendChild(li1);
-favoriteEl.appendChild(li2);
-favoriteEl.appendChild(li3);
-
-li1.textContent = answers[numAnswers];
-li2.textContent = answers[numAnswers + 1];
-li3.textContent = answers[numAnswers + 2];
-
-li1.setAttribute("style", "margin-left: 10%");
-li2.setAttribute("style", "margin-left: 10%");
-li3.setAttribute("style", "margin-left: 10%");
-
-li1.setAttribute("class", "answers");
-li2.setAttribute("class", "answers");
-li3.setAttribute("class", "answers");
-
-// // check answer
-// var getAnswers = document.querySelector(".answers");
-
-// getAnswers.addEventListener("click", function (event) {
-//   var answers = event.target;
-
-//   if (answers.matches(".box")) {
-//     var state = answers.getAttribute("data-state");
-
-//     // Use an if statement to conditionally render the number on the card
-//     if (state === "hidden") {
-//       // If the card is clicked while the state is "hidden", we set .textContent to the number
-//       answers.textContent = answers.dataset.number;
-//       // Using the dataset property, we change the state to visible because the user can now see the number
-//       answers.dataset.state = "visible";
-//     } else {
-//       // 'Hide' the number by setting .textContent to an empty string
-//       answers.textContent = "";
-//       // Use .setAttribute() method
-//       answers.setAttribute("data-state", "hidden");
-//     }
-//   }
-// });
-
-// Length must increase to keep correct answer with the question
-// check answer add points for correct and remove time for incorrect
-function nextQuestion() {
-  for (var i = 5; i < quizQuestion.length; i--) {
-    quizQuestion = question[i];
-    quizAnswers = answers[i];
-    console.log(quizQuestion);
-    console.log(quizAnswers);
-
-    if (quizQuestion === quizAnswers) {
-      totalPoints++;
-      genQuestion();
-    } else {
-      totalPoints--;
-      genQuestion();
-      timerCount - 10;
-    }
-    localStorage.setItem("Points", totalPoints);
-  }
+  nextRound();
 }
 
 startButton.addEventListener("click", startGame);
 
-//   document.getElementById("answers").click();
-getAnswers.addEventListener("click", nextQuestion);
-
-console.log(totalPoints);
+//   localStorage.setItem("Points", totalPoints);
